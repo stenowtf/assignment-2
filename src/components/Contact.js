d6df05dc4e325d4ee2235e6fd95a7f9a7959bf08
Stefano Canducci <steno@fastmail.fm>
2017-07-01 18:01:25 +0200

    again, more styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,19 +7,19 @@ const Contact = () => {
   return (
     <section className="contact">
       <h1>Contact Us</h1>
-      <p>For more information about the services we offer, and how we can work with you please provide us with the following items of information.</p>
-      <p>Fields marked by * are required</p>
+      <h2>For more information about the services we offer, and how we can work with you, please provide us with the following information.</h2>
+      <p>Fields marked by <span className="asterisc">*</span> are required</p>
 
       <Form>
-        <Form.Field width={6}>
+        <Form.Field>
           <input placeholder='Your name' />
-          <label><span>*</span> First name and last name</label>
+          <label><span className="asterisc">*</span> First name and last name</label>
         </Form.Field>
-        <Form.Field width={6}>
+        <Form.Field>
           <input placeholder='Email address' />
-          <label><span>*</span> Response will be sent to this email</label>
+          <label><span className="asterisc">*</span> Response will be sent to this email</label>
         </Form.Field>
-        <Form.Field width={6}>
+        <Form.Field>
           <input placeholder='Phone number' />
           <label>We’ll call you if you prefer</label>
         </Form.Field>
